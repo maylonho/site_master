90e5115df6510d0d2154d34facb74e1754b1beb6
maylonho <oliveira_maylon@hotmail.com>
2021-11-09 11:51:34 -0300

    m

---
+++ js/funcoes.js
@@ -0,0 +1,6 @@
+function link_zap(){
+    location.href='https://api.whatsapp.com/send?phone=5513997191911';
+}
+var tel_zap = document.getElementsByClassName("zap")[0];
+    tel_zap.addEventListener("click", link_zap);
+
