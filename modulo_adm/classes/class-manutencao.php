c32a6eece34a08f533bfc8574ef6e6502ac264e3
maylonho <oliveira_maylon@hotmail.com>
2023-01-20 16:45:44 -0300

    m

--- modulo_adm/classes/class-manutencao.php
+++ modulo_adm/classes/class-manutencao.php
@@ -10,12 +10,12 @@ class Servicos {
         <table class='table'>
             <thead>
             <tr>
-                <!-- <th class='col-md-2 d-none d-md-table-cell' scope='col'>Data</th> -->
+                <th class='col-md-2 d-none d-md-table-cell' scope='col'>Data</th>
                 <th class='col-md-1 d-none d-md-table-cell' scope='col'>Modelo</th>
                 <th scope='col'>N° Série</th>
                 <th scope='col'>Defeito</th>
                 <th scope='col'>Solução</th>
-                <th scope='col'>Status</th>
+                <!-- <th scope='col'>Status</th> -->
             </tr>
             </thead>
             <tbody>";
@@ -45,12 +45,12 @@ class Servicos {
             echo 
             "
                 <tr class='linha_tabela $cor_linha' onmouseover=setAttribute('id','$corlinhaon') onmouseout=setAttribute('id','$cor_linha') onclick=location.href='cadManut.php?$linkedit'>
-                    <!-- <td class='col-md-2 d-none d-md-table-cell'>".$data_servico."</td> --> 
+                    <td class='col-md-2 d-none d-md-table-cell'>".$data_servico."</td>
                     <td class='col-md-1 d-none d-md-table-cell'>".$modelo_servico."</td>
                     <td class='col-1'>".$numero_serie_servico."</td>
                     <td class='col-4'>".$defeito_servico."</td>
                     <td class='col-4'>".$solucao_servico."</td>
-                    <td class='col-4'>".$status_servico."</td>
+                    <!-- <td class='col-4'>".$status_servico."</td> --> 
                 </tr>
             
             ";
